c010d35f65d83572fb4fc894557a65bff430231f
MaoKaneda <maddieqqie@gmail.com>
2025-06-12 20:10:10 +0900

    commit

--- app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecordAdd commentMore actions
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-end
