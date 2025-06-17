6ea5303d7a78e5a2b45faae8f1a37e20a8fe08ef
MaoKaneda <maddieqqie@gmail.com>
2025-06-17 14:56:06 +0900

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_13_105425) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_13_121738) do
   create_table "items", charset: "utf8mb3", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -26,19 +26,19 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_13_105425) do
   end
 
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.date "birth_date", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
