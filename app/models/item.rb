737703346f099de11b0572fbe1b4eccde410178f
MaoKaneda <maddieqqie@gmail.com>
2025-06-18 21:57:47 +0900

    commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,15 @@
 class Item < ApplicationRecord
   has_one_attached :image
-end
+  belongs_to :user
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :shipping_day
 
-extend ActiveHash::Associations::ActiveRecordExtensions
-belongs_to :category
-belongs_to :condition
-belongs_to :delivery_fee
-belongs_to :prefecture
-belongs_to :shipping_day
-validates :image, presence: true
+  validates :image, presence: true
   validates :name, presence: true
   validates :description, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
@@ -18,5 +18,5 @@ validates :image, presence: true
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :price, presence: true,
-  numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
