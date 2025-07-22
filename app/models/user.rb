a5b01fa1f2060c88e981de7d65bb35af5b5e5d9d
MaoKaneda <maddieqqie@gmail.com>
2025-07-22 22:54:42 +0900

    Add: User model validations for nickname, names, birth_date and password format

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
 
   has_many :items
   has_many :orders
+
+  validates :nickname, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龠々ー]+\z/, message: 'is invalid' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龠々ー]+\z/, message: 'is invalid' }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid' }
+  validates :birth_date, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid' }
 end
