c2d57d861dd76acf5993a47f5323f2c653807c14
MaoKaneda <maddieqqie@gmail.com>
2025-07-22 22:53:32 +0900

    Fix: Address all review comments - edit access control, remove debug logs, add associations, sold out display, and improve tests

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,9 +22,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-
-
-    redirect_to root_path
+    if current_user != @item.user || @item.order.present?
+      redirect_to root_path
+    end
   end
 
   def update
