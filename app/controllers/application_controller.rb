24a65154ff73f33bae38821c0aad3bbee626954e
MaoKaneda <maddieqqie@gmail.com>
2025-07-24 01:18:45 +0900

    Restore all files to original state

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-  # 一時的にBasic認証を無効化
-  # before_action :basic_auth
+  before_action :basic_auth
 
   private
 
@@ -8,10 +7,5 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
-  end
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date])
   end
 end
