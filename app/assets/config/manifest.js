2c4bb1dcb1223ab3391c1f1baa01f4da6294cfc1
MaoKaneda <maddieqqie@gmail.com>
2025-07-23 16:06:05 +0900

    Fix: Simplify manifest.js to only include essential files

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,2 @@
 //= link application.js
-//= link controllers/application.js
-//= link controllers/hello_controller.js
-//= link controllers/index.js
-//= link items_price.js
-//= link card.js
 //= link application.css
