ea63ec63b138a12eac90e7fe8bebd4326e666cd9
MaoKaneda <maddieqqie@gmail.com>
2025-07-24 01:47:31 +0900

    Force Basic Auth with manual header check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,15 @@ class ApplicationController < ActionController::Base
     Rails.logger.info "ENV['BASIC_AUTH_USER']: #{ENV['BASIC_AUTH_USER']}"
     Rails.logger.info "ENV['BASIC_AUTH_PASSWORD']: #{ENV['BASIC_AUTH_PASSWORD']}"
     Rails.logger.info "Request headers: #{request.headers['HTTP_AUTHORIZATION']}"
+    Rails.logger.info "User-Agent: #{request.headers['HTTP_USER_AGENT']}"
+
+    # 強制的にBasic認証を要求
+    unless request.headers['HTTP_AUTHORIZATION']
+      Rails.logger.info 'No authorization header found - requesting Basic Auth'
+      response.headers['WWW-Authenticate'] = 'Basic realm="FURIMA"'
+      render plain: 'Authentication required', status: :unauthorized
+      return
+    end
 
     authenticate_or_request_with_http_basic('FURIMA') do |username, password|
       Rails.logger.info "Auth attempt - username: #{username}, password: #{password}"
