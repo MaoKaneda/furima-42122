bb90f391b132aec63c46381dccb8dcd9c1f18da8
MaoKaneda <maddieqqie@gmail.com>
2025-07-24 01:46:24 +0900

    Add detailed Basic Auth debugging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,14 @@ class ApplicationController < ActionController::Base
     Rails.logger.info '=== Basic Auth Debug ==='
     Rails.logger.info "ENV['BASIC_AUTH_USER']: #{ENV['BASIC_AUTH_USER']}"
     Rails.logger.info "ENV['BASIC_AUTH_PASSWORD']: #{ENV['BASIC_AUTH_PASSWORD']}"
+    Rails.logger.info "Request headers: #{request.headers['HTTP_AUTHORIZATION']}"
 
     authenticate_or_request_with_http_basic('FURIMA') do |username, password|
       Rails.logger.info "Auth attempt - username: #{username}, password: #{password}"
       result = username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
       Rails.logger.info "Auth result: #{result}"
+      Rails.logger.info "Expected username: #{ENV['BASIC_AUTH_USER']}, got: #{username}"
+      Rails.logger.info "Expected password: #{ENV['BASIC_AUTH_PASSWORD']}, got: #{password}"
       result
     end
   end
