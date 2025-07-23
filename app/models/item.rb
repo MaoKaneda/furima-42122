4bc0eec734e1ae3253a703aa951157228474fe6d
MaoKaneda <maddieqqie@gmail.com>
2025-07-23 16:09:43 +0900

    Fix: Add user presence validation to Item model and fix test

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,6 +12,7 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :name, presence: true
   validates :description, presence: true
+  validates :user, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :delivery_fee_id, numericality: { other_than: 1, message: "can't be blank" }
