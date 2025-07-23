a3afafc09686ad28a04a7e6278fd2337a4151822
MaoKaneda <maddieqqie@gmail.com>
2025-07-24 01:49:40 +0900

    Clean up Basic Auth code - remove debug logs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,27 +4,8 @@ class ApplicationController < ActionController::Base
   private
 
   def basic_auth
-    Rails.logger.info '=== Basic Auth Debug ==='
-    Rails.logger.info "ENV['BASIC_AUTH_USER']: #{ENV['BASIC_AUTH_USER']}"
-    Rails.logger.info "ENV['BASIC_AUTH_PASSWORD']: #{ENV['BASIC_AUTH_PASSWORD']}"
-    Rails.logger.info "Request headers: #{request.headers['HTTP_AUTHORIZATION']}"
-    Rails.logger.info "User-Agent: #{request.headers['HTTP_USER_AGENT']}"
-
-    # 強制的にBasic認証を要求
-    unless request.headers['HTTP_AUTHORIZATION']
-      Rails.logger.info 'No authorization header found - requesting Basic Auth'
-      response.headers['WWW-Authenticate'] = 'Basic realm="FURIMA"'
-      render plain: 'Authentication required', status: :unauthorized
-      return
-    end
-
     authenticate_or_request_with_http_basic('FURIMA') do |username, password|
-      Rails.logger.info "Auth attempt - username: #{username}, password: #{password}"
-      result = username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
-      Rails.logger.info "Auth result: #{result}"
-      Rails.logger.info "Expected username: #{ENV['BASIC_AUTH_USER']}, got: #{username}"
-      Rails.logger.info "Expected password: #{ENV['BASIC_AUTH_PASSWORD']}, got: #{password}"
-      result
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 end
